4ee6571eff55960661455a079500335198a0b3c3
AymanMagdy <magdia883@gmail.com>
2020-02-01 19:09:05 +0200

    insert

--- Insert-Data.sh
+++ Insert-Data.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
+shopt -s extglob
+LC_ALL=C
 
 function InsertData () {
     mapfile -t arrayMeta < data/$currentDB/meta-data/$currentTB.md
     data=""
     for (( i=0; i<${#arrayMeta[@]}; i++ ))
     do 
-        dataType=$(awk -v i="$i" -F: '{if(NR == i+1) print $1}' data/$currentDB/meta-data/$currentTB.md)
-        echo "Enter the ${dataType}"
+        colName=$(awk -v i="$i" -F: '{if(NR == i+1) print $1}' data/$currentDB/meta-data/$currentTB.md)
+        dataType=$(awk -v i="$i" -F: '{if(NR == i+1) print $2}' data/$currentDB/meta-data/$currentTB.md)
+
+        echo "Enter the ${colName}"
         read colValue;
-        if [ $i -gt 0 ]
+
+        if [ ${dataType} == "Numbers" ]
         then 
-            data+=":"
+            case $colValue in 
+            +([0-9]))
+                $data
+                data+=$colValue;
+                ;;
+            *)
+                echo "The id should be integer"
+                ;;
+            esac
+        elif [ ${dataType} == "alphabet letters" ]
+        then 
+            if [ $i -gt 0 ]
+            then 
+                data+=":"
+            fi
+            case $colValue in 
+            +([a-zA-Z]))
+                data+=$colValue;
+                ;;
+            *)
+                echo "The id should be alphabet letters"
+                ;;
+            esac
+        else
+            if [ $i -gt 0 ]
+            then 
+                data+=":"
+            fi
+            data+=$colValue;
         fi
-        data+=$colValue
     done
     echo $data >> data/$currentDB/tables/$currentTB
     echo "Done adding the data to table '${currentTB}'"
