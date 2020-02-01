7e6e4bc9498256d1cff4bad7a1d011aaf01f40aa
MinaMaher0 <minamaher30697@gmail.com>
2020-02-01 23:35:09 +0200

    select based on PK

---
+++ Select-Based-PK.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+function SelectRecord {
+    primaryKeyFieledNum=`awk -F: '{if ($3==1) print NR;}' data/$currentDB/meta-data/$currentTB.md` 
+    primaryKeyFieledName=`awk -F: '{if ($3==1) print $1;}' data/$currentDB/meta-data/$currentTB.md` 
+    read -p "Enter $primaryKeyFieledName value to select the record : " val 
+
+    isExist=$(awk -F: -v fieledNum=$primaryKeyFieledNum -v fieledVal=$val 'BEGIN{isFounded=0;} {if ($fieledNum==fieledVal){ isFounded=1; }} END {print isFounded;}' data/$currentDB/tables/$currentTB)
+    
+    if [[ $isExist == 1 ]]
+    then 
+        recordNumber=$(awk -F: -v fieledNum=$primaryKeyFieledNum -v fieledVal=$val 'BEGIN{recordNumber=0;} {if ($fieledNum==fieledVal){ recordNumber=NR; }} END {print recordNumber;}' data/$currentDB/tables/$currentTB)
+        sed -n "$recordNumber p" data/$currentDB/tables/$currentTB
+    else
+        echo "${red}Not found any record match $val${reset}"
+    fi
+}
+
+SelectRecord
